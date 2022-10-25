25d4e16d7e1c6a6846c0626a49ab67b2edecd7ec
Roulianosss <julien.ortscheid@gmail.com>
2022-10-25 08:01:55 +0200

    added archives feature

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ const bugsList = [{
     "environement": "frontend",
     "description": "test de bug",
     "priority": "1",
-    "date": "24/10/2022 17:12:38",
-    'number': 2
-
+    "date": "24/10/2022 17:12:38"
     }
 ]
 
-//Formulaire de creation
+const archivedList = [{
+    "id": "test",
+    "name": "test",
+    "environement": "frontend",
+    "description": "test de bug",
+    "priority": "1",
+    "date": "24/10/2022 17:12:38"
+    }
+]
+
+//Formulaire de creation de bug
 const form = document.querySelector('form')
 form.addEventListener('submit', handleForm)
 function handleForm(e) {
@@ -19,7 +27,7 @@ function handleForm(e) {
     form.reset()
 }
 //fonction de creation apres remplissage formulaire
-function createBug(name, environement, description, priority) {
+function createBug(name, description, environement, priority) {
     bugsList.push({
         id: crypto.randomUUID(),
         name: name,
@@ -37,19 +45,68 @@ function displayBugsList() {
     for (let bug of bugsList) {
         displayBugs.innerHTML += `
         <div class='row' id='${bug.id}'>
-            <div class='delete-bug' id=${bug.id}>✖</div>
             <p>${bug.id.slice(0, 6)}</p>
             <p>${bug.date}</p>
             <p>${bug.name}</p>
             <p>${bug.description}</p>
             <p>${bug.environement}</p>
             <p>${bug.priority}</p>
+            <div class="btn-crud-container">
+                <div class='edit-bug' id=${bug.id}>✏️</div>
+                <div class='archive-bug' id=${bug.id}>📁</div>
+                <div class='delete-bug' id=${bug.id}>❌</div>
+            
+            </div>
         </div>
         `
+        const allEditBtn = document.querySelectorAll('.edit-bug')
+        allEditBtn.forEach(btn => btn.addEventListener('click', handleEdit))
+        const allArchiveBtn = document.querySelectorAll('.archive-bug')
+        allArchiveBtn.forEach(btn => btn.addEventListener('click', handleArchive))
         const allRemoveBtn = document.querySelectorAll('.delete-bug')
         allRemoveBtn.forEach(btn => btn.addEventListener('click', handleRemove))
     }
+    // affichage archived list
+    const displayArchive = document.querySelector('#rows-archives')
+    displayArchive.innerHTML = ''
+    for (let bug of archivedList) {
+        displayArchive.innerHTML += `
+        <div class='row' id='${bug.id}'>
+            <p>${bug.id.slice(0, 6)}</p>
+            <p>${bug.date}</p>
+            <p>${bug.name}</p>
+            <p>${bug.description}</p>
+            <p>${bug.environement}</p>
+            <p>${bug.priority}</p>
+            <div class="btn-crud-container">
+                <div class='edit-bug' id=${bug.id}>✏️</div>
+                <div class='archive-bug' id=${bug.id}>📁</div>
+                <div class='delete-bug' id=${bug.id}>❌</div>
+            
+            </div>
+        </div>
+        `
+        // const allEditBtn = document.querySelectorAll('.edit-bug')
+        // allEditBtn.forEach(btn => btn.addEventListener('click', handleEdit))
+        // const allArchiveBtn = document.querySelectorAll('.archive-bug')
+        // allArchiveBtn.forEach(btn => btn.addEventListener('click', handleArchive))
+        // const allRemoveBtn = document.querySelectorAll('.delete-bug')
+        // allRemoveBtn.forEach(btn => btn.addEventListener('click', handleRemove))
+    }
 } 
+//fonction edition
+function handleEdit() {
+
+}
+//fontion archivage
+function handleArchive(e) {
+    const archiveIndex = bugsList.findIndex(bug => bug.id === e.target.getAttribute('id'))
+    archivedList.push(bugsList[archiveIndex])
+    bugsList.splice(archiveIndex, 1)
+    displayBugsList()
+}
+
+
 //fonction de gestion de la suppression
 function handleRemove(e) {
     const removeIndex = bugsList.findIndex(bug => bug.id === e.target.getAttribute('id'))
@@ -60,14 +117,19 @@ function handleRemove(e) {
 const sortBtns = document.querySelectorAll('.sort-btn')
 sortBtns.forEach(btn => btn.addEventListener('click', (e) => sortBugs(e.target.getAttribute('data-attr'))))
 let toggleSort = false
+const btnsArray = [...sortBtns]
 function sortBugs(type) {
+    btnsArray.forEach(btn => btn.textContent = btn.textContent.replace(/[▾▴]/gm, ''))
     if (!toggleSort) {
         bugsList.sort((a,b) => (a[type] > b[type]) ? 1 : ((b[type] > a[type]) ? -1 : 0))
         toggleSort = true
+        btnsArray.find(btn => btn.getAttribute('data-attr') === type).textContent += '▴'
     } else {
         bugsList.sort((b,a) => (a[type] > b[type]) ? 1 : ((b[type] > a[type]) ? -1 : 0))
         toggleSort = false
+        btnsArray.find(btn => btn.getAttribute('data-attr') === type).textContent += '▾'
     }
+    console.log([...sortBtns].find(btn => btn.getAttribute('data-attr') === type));
     displayBugsList()
 }
 
